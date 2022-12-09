3550efa9fdb9189bdc48ca303a5a92b155558141
Evgeniy Alehnovich <evgeniy_alehnovich@proton.me>
2022-12-09 16:36:39 +0300

    add card admin

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,56 @@
-from django.contrib import admin
+import datetime
 
-# Register your models here.
+from django.contrib import admin
+from babel.dates import format_date
+
+from .models import Card
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    readonly_fields = ('formatted_started_at', 'formatted_finished_at',)
+    list_display = (
+        'card',
+        'owner',
+        'formatted_started_at',
+        'formatted_finished_at',
+        'is_active',
+    )
+    list_filter = ('owner', 'is_active')
+    search_fields = (
+        'series',
+        'number',
+        'created_at',
+        'is_active',
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('series', 'number', 'is_active',),
+                ('owner', 'discount_amount',),
+            )
+        }),
+        (None, {
+            'fields':(
+                ('card_experation', 'formatted_started_at', 'formatted_finished_at',),
+            ),
+        })
+    )
+
+    @admin.display(description='Дата начала действия карты')
+    def formatted_started_at(self, obj):
+        return format_date(obj.created_at, 'E, d MMMM yyyy', locale='ru')
+
+    @admin.display(description='Дата окончания действия карты')
+    def formatted_finished_at(self, obj):
+        if obj.owner:
+            # created_at = obj.created_at
+            # finished_at = created_at + datetime.timedelta(days=30 * int(obj.card_experation))
+            return format_date(obj.finished_at(), 'E, d MMMM yyyy', locale='ru')
+
+    @admin.display(description='Карта')
+    def card(self, obj):
+        if obj.owner:
+            return f'{obj.series}{obj.number}'
+
--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,71 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import (
+    RegexValidator,
+    MinValueValidator,
+    MaxValueValidator,
+)
+
+
+class Card(models.Model):
+    CARD_EXPERATION = (
+        ('1', '1 Месяц'),
+        ('6', '6 Месяцев'),
+        ('12', '12 Месяцев'),
+    )
+
+    series = models.CharField(
+        'Серия карты',
+        max_length=4,
+        validators=[
+            RegexValidator(
+                regex='[A-Z]{4}',
+                message='Серия карты должна содержать 4 латинских символа в верхнем регистре'
+            )
+        ]
+    )
+    number = models.PositiveSmallIntegerField('Номер карты')
+    owner = models.ForeignKey(
+        User,
+        related_name='cards',
+        on_delete=models.CASCADE,
+        verbose_name='Владелец'
+    )
+    discount_amount = models.PositiveSmallIntegerField(
+        'Процент скидки',
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(100)
+        ]
+    )
+    is_active = models.BooleanField(
+        'Карта активна?',
+        default=False
+    )
+    created_at = models.DateTimeField(
+        'Дата создания карты',
+        auto_now_add=True
+    )
+    card_experation = models.CharField(
+        'Срок действия',
+        max_length=15,
+        choices=CARD_EXPERATION,
+        default='1'
+    )
+
+    class Meta:
+        unique_together = 'series', 'number',
+        index_together = 'series', 'number',
+        ordering = '-created_at',
+        verbose_name = 'Карта'
+        verbose_name_plural = 'Карты'
+
+    def __str__(self):
+        return f'{self.series}{self.number} - {self.owner}'
+
+    def finished_at(self):
+        return self.created_at + datetime.timedelta(
+            days=30 * int(self.card_experation)
+        )
